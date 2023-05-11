f6e386e297b65bbd23876a7cee8fa2316df88030
unknown <andreyxdd@yandex.ru>
2023-05-11 15:44:33 -0600

    Set up eslint and typescript configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="application-name" content="PWA App" />
+        <meta name="application-name" content="Toncoin Tracker" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
+        <meta name="apple-mobile-web-app-title" content="Toncoin Tracker" />
+        <meta name="description" content="Simple PWA that plots toncoin historical data" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -55,17 +57,17 @@ export default function Document() {
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="PWA App" />
-        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta name="twitter:title" content="Toncoin Tracker" />
+        <meta name="twitter:description" content="Simple PWA that plots toncoin historical data" />
         <meta
           name="twitter:image"
           content="https://yourdomain.com/icons/android-chrome-192x192.png"
         />
         <meta name="twitter:creator" content="@imamdev_" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="PWA App" />
-        <meta property="og:description" content="Best PWA App in the world" />
-        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:title" content="Toncoin Tracker" />
+        <meta property="og:description" content="Simple PWA that plots toncoin historical data" />
+        <meta property="og:site_name" content="Toncoin Tracker" />
         <meta property="og:url" content="https://yourdomain.com" />
         <meta
           property="og:image"
@@ -77,5 +79,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
